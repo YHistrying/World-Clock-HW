03c605fcabafc3cf93c57307a15a1f1b95d491af
yijenislearningtocode <yijenhs@gmail.com>
2025-07-23 20:03:22 +0200

    Added Select functionality

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 function updateClock() {
-  let londonElement = document.getElementById("#london");
   let londonDateElement = document.querySelector("#london .date");
   let londonTimeElement = document.querySelector("#london .time");
   let londonTime = moment().tz("Europe/London");
-  londonDateElement.innerText = londonTime.format("MMMM Do YYYY");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
   londonTimeElement.innerHTML = londonTime.format(
     "hh:mm:ss [<small>]A[</small>]"
   );
 
-  let tokyoElement = document.getElementById("#tokyo");
   let tokyoDateElement = document.querySelector("#tokyo .date");
   let tokyoTimeElement = document.querySelector("#tokyo .time");
   let tokyoTime = moment().tz("Asia/Tokyo");
-  tokyoDateElement.innerText = tokyoTime.format("MMMM Do YYYY");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format(
     "hh:mm:ss [<small>]A[</small>]"
   );
@@ -20,3 +18,24 @@ function updateClock() {
 
 updateClock();
 setInterval(updateClock, 1000);
+
+function changeCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityElement = document.querySelector("#chosen-city");
+  let cityTimeNow = moment().tz(cityTimeZone);
+  let cityDateNow = cityTimeNow.format("MMMM Do YYYY");
+  let cityTimeNowFormatted = cityTimeNow.format("hh:mm:ss");
+  let cityTimeNowAmPm = cityTimeNow.format("A");
+
+  cityElement.innerHTML = `<div class="city">
+      <div>
+          <h2 class="city-name">${cityName}</h2>
+          <div class="date">${cityDateNow}</div>
+        </div>
+        <div class="time">${cityTimeNowFormatted} <small>${cityTimeNowAmPm}</small></div>
+    </div>`;
+}
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", changeCity);
